d093fcf4dd27f1a64a456f095529aa337efb56c4
DarthNihilus15 <62901470+DarthNihilus15@users.noreply.github.com>
2020-04-02 11:44:09 -0400

    Changed to accomodate project and improve upon the game.

--- main.ts
+++ main.ts
@@ -19,25 +19,6 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `
     //% blockIdentity=images._tile
-    export const tile1 = img`
-7 7 7 7 f f f f f f f 7 7 7 7 7 
-7 7 f f 7 7 7 f 7 7 7 f f 7 7 7 
-7 f 7 7 7 7 7 f 7 7 7 7 7 f 7 7 
-7 f 7 7 f f f f f f f 7 7 f 7 7 
-f 7 7 f 7 7 7 f 7 7 7 f 7 7 f 7 
-f 7 7 f 7 f f f f f 7 f 7 7 f 7 
-f 7 7 f 7 f 7 f 7 f 7 f 7 7 f 7 
-f f f f f f f f f f f f f f f 7 
-f 7 7 f 7 f 7 f 7 f 7 f 7 7 f 7 
-f 7 7 f 7 f f f f f 7 f 7 7 f 7 
-f 7 7 f 7 7 7 f 7 7 7 f 7 7 f 7 
-7 f 7 7 f f f f f f f 7 7 f 7 7 
-7 f 7 7 7 7 7 f 7 7 7 7 7 f 7 7 
-7 7 f f 7 7 7 f 7 7 7 f f 7 7 7 
-7 7 7 7 f f f f f f f 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-`
-    //% blockIdentity=images._tile
     export const tile2 = img`
 b b b b b b b b b b b b b b b b 
 b 1 f 1 f 1 f 1 f 1 f 1 f 1 f b 
@@ -75,38 +56,95 @@ e f e e e e e e e e e e e e f e
 e f f f f f f f f f f f f f f e 
 e e e e e e e e e e e e e e e e 
 `
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+d d d d d d d d d d d d 5 d d d 
+d 5 d d d 5 d d d 5 d d d d d 5 
+d d d 5 d d d d d d d d 5 d d d 
+d d d d d d 5 d d 5 d d d 5 d d 
+d 5 d 5 d d d d d d d d 5 d d d 
+d d d d d d d d 5 d d d d d 5 d 
+d 5 d d d 5 d d d d d d d d d d 
+d d d d d d d d d d 5 d d 5 d d 
+d d 5 d 5 d d 5 d d d d d d d 5 
+d d d d d d d d 5 d d 5 d d d d 
+d 5 d 5 d d d d d d d d d d d d 
+d d d d d d d 5 d 5 d d d 5 d d 
+d 5 d d 5 d d d d d d 5 d d d d 
+d d d d d d d d d d d d d d d 5 
+d 5 d d d d d 5 d d 5 d 5 d d d 
+d d d d d 5 d d 5 d d d d d d d 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f 1 f f f f f 1 f f f f f f f 
+f f f f f f f f f f f f f f 1 f 
+f f f f f f f f f f f f f f f f 
+f f f 1 f f f f 1 f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f 1 f f f f f f f f f f f 
+f f f f f f f f f f f f f 1 f f 
+f f f f f f f f f 1 f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f 1 f f f f f f f f f 
+f f f 1 f f f f f f f f 1 f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`
 }
 function startGame () {
     tiles.setTilemap(tiles.createTilemap(
-            hex`3c0019000a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a010303030a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a03070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a030a070a0a0a160a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a01030303030a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a160a030a0a0a070a0a0a030a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a16070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a01030a030a030a0a030a0a0a0a0a070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0103020a0a0a0a0a0a0a0a0a0a0a0a0a0a0a16070a0a0a0a0a0a0a0a0a0a0a0a0a07160a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a160a0a0a0a0a0a0a0a0a0a0a0a0a060a0a0a010a0a010303020a0a0a0a0a0a0a0a0a0a0a0a0a07030a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0103030303060a0a0a0a0a0a0a0a0a0a08161616070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a160a0a0a070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a02030303020a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a070a0a0a160a0a0a0a0a0a0a0a0a0a010303060a070a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a070a0a0a030a0a0a0a0a150a0a0a090a090a09090709160916090a0a0a0a160a0a0a0a0a160a0a0a0a0a160a0a0a0a160a0a160a0a0a160a0a0a160a0716160a0a0a0a0a0a14030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`,
+            hex`3c001900161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161b19191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191b16161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191719191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191917171717171919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191b1919191919191919191919191919191b19191919191919191919191919191b191919191919191919191919191b1919191616191919171717171717171919191919191917171717171717171719191919191919191717171717171719191919191919171717171717171919191616191919171a1a1a1a1a17191919191919191919171a1a1a1a1a171919191919191919171a1a1a1a1a1719191919191919171a1a1a1a1a171919191616191919171a1a1a1a1a17191919191919191919171a1a1a1a1a171919191919191919171a1a1a1a1a1719191919191919171a1a1a1a1a171919191616191919171a1a1a1a1a17191919191919191919171a1a1a1a1a171919191919191919171a1a1a1a1a1719191919191919171a1a1a1a1a171919191616191919171a1a1a1a1a17191919191917171717171a1a1a1a1a171919191919191919171a1a1a1a1a1719191919191919171a1a1a1a1a171919191616191919171a1a1a1a1a17191919191919191919171a1a1a1a1a171919191919191919171a1a1a1a1a1719191919191919171a1a1a1a1a1719191916161919191717171717171719191919191919191917171717171717191919191919191917171717171717191919191919191717171717171719191916161919191919191919191919191717171717171719191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191917171719191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191717171919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191916161419191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191919191b16161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616161616`,
             img`
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 2 2 . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 2 . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 . 2 . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . 2 2 2 2 2 . . . . . . . . . . . . . . . . . . . . . . 2 . . . 2 . . . 2 . . . . . 
-. . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . . . . 2 2 . 2 . 2 . . 2 . . . . . 2 . . . . . . . . . 
-. . . . . . . . . . . . . . . . . 2 2 2 . . . . . . . . . . . . . . . . 2 . . . . . . . . . . . . . 2 . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . 2 . . 2 2 2 2 . . . . . . . . . . . . . 2 2 . . . . . . . . 
-. . . . . . . . . . 2 2 2 2 2 2 . . . . . . . . . . 2 . . . 2 . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . 
-. . . . . . . . . . 2 . . . . . . . . . . . . . . . 2 2 2 2 2 . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . 
-. . . . . 2 2 2 2 . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . 2 . . . . . 
-. . . . 2 . 2 . 2 2 2 2 . 2 . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . 2 2 2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 2 2 2 2 . . . . . . . 2 2 2 2 2 2 2 2 2 . . . . . . . . 2 2 2 2 2 2 2 . . . . . . . 2 2 2 2 2 2 2 . . . 2 
+2 . . . 2 . . . . . 2 . . . . . . . . . 2 . . . . . 2 . . . . . . . . 2 . . . . . 2 . . . . . . . 2 . . . . . 2 . . . 2 
+2 . . . 2 . . . . . 2 . . . . . . . . . 2 . . . . . 2 . . . . . . . . 2 . . . . . 2 . . . . . . . 2 . . . . . 2 . . . 2 
+2 . . . 2 . . . . . 2 . . . . . . . . . 2 . . . . . 2 . . . . . . . . 2 . . . . . 2 . . . . . . . 2 . . . . . 2 . . . 2 
+2 . . . 2 . . . . . 2 . . . . . 2 2 2 2 2 . . . . . 2 . . . . . . . . 2 . . . . . 2 . . . . . . . 2 . . . . . 2 . . . 2 
+2 . . . 2 . . . . . 2 . . . . . . . . . 2 . . . . . 2 . . . . . . . . 2 . . . . . 2 . . . . . . . 2 . . . . . 2 . . . 2 
+2 . . . 2 2 2 2 2 2 2 . . . . . . . . . 2 2 2 2 2 2 2 . . . . . . . . 2 2 2 2 2 2 2 . . . . . . . 2 2 2 2 2 2 2 . . . 2 
+2 . . . . . . . . . . . . 2 2 2 2 2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . 2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . 2 2 2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 2 
 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
 `,
-            [myTiles.tile0,sprites.castle.tilePath1,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.castle.tilePath7,myTiles.tile1,sprites.castle.tilePath3,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.builtin.forestTiles0,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass1,sprites.builtin.forestTiles5,sprites.builtin.forestTiles2,sprites.builtin.forestTiles28,sprites.builtin.forestTiles20,sprites.castle.tileGrass3,myTiles.tile2,sprites.dungeon.stairLarge,myTiles.tile3],
+            [myTiles.tile0,sprites.castle.tilePath1,sprites.castle.tilePath5,sprites.castle.tilePath2,sprites.castle.tilePath7,sprites.castle.tilePath3,sprites.castle.tilePath4,sprites.castle.tilePath6,sprites.builtin.forestTiles0,sprites.castle.tileGrass1,sprites.castle.tileGrass2,sprites.castle.tileDarkGrass2,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass1,sprites.builtin.forestTiles5,sprites.builtin.forestTiles2,sprites.builtin.forestTiles28,sprites.builtin.forestTiles20,sprites.castle.tileGrass3,myTiles.tile2,sprites.dungeon.stairLarge,myTiles.tile3,sprites.dungeon.floorDark2,sprites.dungeon.floorDark0,myTiles.tile5,myTiles.tile6,myTiles.tile7,sprites.dungeon.darkGroundCenter],
             TileScale.Sixteen
         ))
     Skull_Trooper = sprites.create(img`
@@ -127,26 +165,38 @@ function startGame () {
 f f f f f f f f . . f d f d f 
 f f f f f f . . . . . f . f . 
 `, SpriteKind.Player)
-    controller.moveSprite(Skull_Trooper, 100, 0)
+    controller.moveSprite(Skull_Trooper, 115, 0)
     Skull_Trooper.ay = 400
     scene.cameraFollowSprite(Skull_Trooper)
     tiles.placeOnRandomTile(Skull_Trooper, sprites.dungeon.stairLarge)
     numJumps = 0
+    info.setLife(3)
+    info.setScore(25)
 }
-scene.onOverlapTile(SpriteKind.Player, myTiles.tile3, function (sprite, location) {
-    info.changeScoreBy(1)
-    tiles.setTileAt(location, sprites.castle.tileGrass1)
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+f f f f f . 
+f f f f f f 
+f f f f f . 
+`, Skull_Trooper, 100, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(-1)
+    otherSprite.destroy()
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (numJumps < 1) {
+    if (numJumps < 2) {
         Skull_Trooper.vy = -200
         numJumps += 1
     }
 })
-scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
-    info.changeScoreBy(30)
-    game.over(true, effects.confetti)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    info.changeScoreBy(-1)
+    otherSprite.destroy()
 })
+let Viper_Legion_Soldier: Sprite = null
+let projectile: Sprite = null
 let numJumps = 0
 let Skull_Trooper: Sprite = null
 scene.setBackgroundImage(img`
@@ -273,6 +323,28 @@ f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 `)
 game.showLongText("get to the finish line at the end using a,d, and space or arrow keys", DialogLayout.Bottom)
 startGame()
+game.onUpdateInterval(5000, function () {
+    Viper_Legion_Soldier = sprites.create(img`
+. . . . c c c c c c . . . . . . 
+. . . c 6 7 7 7 7 6 c . . . . . 
+. . c 7 7 7 7 7 7 7 7 c . . . . 
+. c 6 7 7 7 7 7 7 7 7 6 c . . . 
+. c 7 c 6 6 6 6 c 7 7 7 c . . . 
+. f 7 6 f 6 6 f 6 7 7 7 f . . . 
+. f 7 7 7 7 7 7 7 7 7 7 f . . . 
+. . f 7 7 7 7 6 c 7 7 6 f c . . 
+. . . f c c c c 7 7 6 f 7 7 c . 
+. . c 7 2 7 7 7 6 c f 7 7 7 7 c 
+. c 7 7 2 7 7 c f c 6 7 7 6 c c 
+c 1 1 1 1 7 6 f c c 6 6 6 c . . 
+f 1 1 1 1 1 6 6 c 6 6 6 6 f . . 
+f 6 1 1 1 1 1 6 6 6 6 6 c f . . 
+. f 6 1 1 1 1 1 1 6 6 6 f . . . 
+. . c c c c c c c c c f . . . . 
+`, SpriteKind.Enemy)
+    tiles.placeOnRandomTile(Viper_Legion_Soldier, sprites.dungeon.darkGroundCenter)
+    Viper_Legion_Soldier.follow(Skull_Trooper)
+})
 forever(function () {
     if (Skull_Trooper.isHittingTile(CollisionDirection.Bottom)) {
         numJumps = 0
